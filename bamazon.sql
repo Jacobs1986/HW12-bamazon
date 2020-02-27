52ee36997cba1a39290acff499b1a27c2cea8b0a
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-27 13:35:49 -0700

    Wrote the function itemSearch that will take in the productID parameter and search the list for the matching item_id. Then pulls the stock_quantity and checks it against the requestedQuantity to see if the order can be fulfilled.

--- bamazon.sql
+++ bamazon.sql
@@ -9,6 +9,20 @@ CREATE TABLE products (
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(100) NOT NUll,
     price DECIMAL(10,2) NOT NULL,
-    stock_quanity INT NOT NULL,
+    stock_quantity INT NOT NULL,
     PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+	("Artist Watercolor Set", "Art Supplies", 30.95, 50),
+	("Leather Carving Set", "Art Supplies", 55.95, 3),
+    ("Tissues", "Home Supplies", 5.95, 100),
+    ("Fine Glassware", "Home Supplies", 6.95, 85),
+    ("A Discovery of Witches", "Books", 16.95, 10),
+    ("Rose Bulbs (pack of 4)", "Gardening", 25.95, 40),
+    ("Fertilizer", "Gardening", 20.95, 15),
+    ("Raise the Titanic", "Books", 10.95, 12),
+    ("Cheese Maker", "Home Supplies", 95.95, 3),
+    ("Decorative Painting", "Home Supplies", 102.95, 1);
+    
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,13 +12,26 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`)
     showTable();
+    itemSearch(3, 102);
     connection.end()
 });
 
 // functions
 showTable = () => {
     connection.query("SELECT * FROM products", (err, res) => {
-        if (err) throw res;
+        if (err) throw err;
         console.table(res);
     });
 };
+
+itemSearch = (productID, requestedQuantity) => {
+    connection.query("SELECT * FROM products WHERE item_id=?", [productID], (err, res) => {
+        if (err) throw err;
+       console.log(res[0].product_name, res[0].stock_quantity);
+       if (requestedQuantity > res[0].stock_quantity) {
+           console.log("There are not enough items to complete this order.")
+       } else {
+           console.log("We will fill this order.")
+       }
+    })
+}
