a9eab6f52c44a23b60aa2f763a633c28ab937a10
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-27 14:17:30 -0700

    Added inquirer prompts to the program so now the user can make selections from the command line.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -12,35 +13,63 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`)
     showTable();
-    itemSearch(3, 50);
+    // itemSearch(3, 50);
     // updateProduct();
 });
 
+// questions for user to select product for purchase
+let questionsSelect = [
+    {
+        type: "number",
+        name: "productID",
+        message: "Select the product you want to purchase by its ID number: "
+    },
+    {
+        type: "number",
+        name: "productQuantity",
+        message: "How much of this product do you need: "
+    }
+];
+
 // functions
+// show the current table
 showTable = () => {
     connection.query("SELECT * FROM products", (err, res) => {
         if (err) throw err;
         console.table(res);
+        userSelect();
     });
 };
 
+// user selects product they want
+userSelect = () => {
+    inquirer.prompt(questionsSelect).then(answers => {
+        // console.log(answers);
+        itemSearch(answers.productID, answers.productQuantity);
+    });
+};
+
+// search for product
 itemSearch = (productID, requestedQuantity) => {
     connection.query("SELECT * FROM products WHERE item_id=?", [productID], (err, res) => {
         if (err) throw err;
-       console.log(res[0].product_name, res[0].stock_quantity);
+    //    console.log(res[0].product_name, res[0].stock_quantity);
        let currentStock = res[0].stock_quantity;
+       let price = res[0].price;
        if (requestedQuantity > currentStock) {
            console.log("There are not enough items to complete this order.")
        } else {
            console.log("We will fill this order.")
-           updateProduct(productID, requestedQuantity, currentStock)
+           updateProduct(productID, requestedQuantity, currentStock, price)
        }
     })
 }
 
-updateProduct = (productID, requestedQuantity, currentStock) => {
+// update product quantity
+updateProduct = (productID, requestedQuantity, currentStock, price) => {
     // console.log(productID, requestedQuantity, currentStock);
     let newStock = currentStock - requestedQuantity;
+    let totalPrice = requestedQuantity * price;
     // console.log(newStock)
     connection.query("UPDATE products SET ? WHERE ?", [
         {
