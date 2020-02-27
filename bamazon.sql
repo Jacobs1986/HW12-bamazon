044567a7ab9f664defefbaed081645c4ba1ebcec
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-27 10:44:47 -0700

    Created the products table and populated it with things to sell.

--- bamazon.sql
+++ bamazon.sql
@@ -3,3 +3,12 @@ DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NOT NUll,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quanity INT NOT NULL,
+    PRIMARY KEY (item_id)
+);
