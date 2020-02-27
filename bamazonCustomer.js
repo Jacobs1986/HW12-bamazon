ff5038bdd1fca35131abeb429917b78a7c345494
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-27 11:09:43 -0700

    I got the layout for the connection between the javascript file and the database.

---
+++ bamazonCustomer.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Hnc13rt!*",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`)
+    connection.end()
+});
