31c7d91614e524067a5676d28cd58f1159b0c4dd
Sean Jacobson <sean.jacobson86@gmail.com>
2020-02-27 13:55:16 -0700

    Created an updateProduct function that will update the number of products in the database.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,8 +12,8 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`)
     showTable();
-    itemSearch(3, 102);
-    connection.end()
+    itemSearch(3, 50);
+    // updateProduct();
 });
 
 // functions
@@ -28,10 +28,31 @@ itemSearch = (productID, requestedQuantity) => {
     connection.query("SELECT * FROM products WHERE item_id=?", [productID], (err, res) => {
         if (err) throw err;
        console.log(res[0].product_name, res[0].stock_quantity);
-       if (requestedQuantity > res[0].stock_quantity) {
+       let currentStock = res[0].stock_quantity;
+       if (requestedQuantity > currentStock) {
            console.log("There are not enough items to complete this order.")
        } else {
            console.log("We will fill this order.")
+           updateProduct(productID, requestedQuantity, currentStock)
        }
     })
 }
+
+updateProduct = (productID, requestedQuantity, currentStock) => {
+    // console.log(productID, requestedQuantity, currentStock);
+    let newStock = currentStock - requestedQuantity;
+    // console.log(newStock)
+    connection.query("UPDATE products SET ? WHERE ?", [
+        {
+            stock_quantity: newStock
+        },
+        {
+            item_id: productID
+        }
+    ],
+    function(err, res) {
+        if (err) throw err;
+        showTable();
+        connection.end()
+    });
+};
